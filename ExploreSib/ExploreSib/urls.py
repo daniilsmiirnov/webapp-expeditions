88643a12dd4aeab7188269de1b3e7c5780705535
daniilsmiirnov <dan_smirnov@mail.ru>
2023-10-22 14:48:06 +0300

    api view merge

--- ExploreSib/ExploreSib/urls.py
+++ ExploreSib/ExploreSib/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from lab import views
+from lab import views1
 from rest_framework import routers
 
 
@@ -26,30 +27,30 @@ urlpatterns = [
 
     # API objects
     path(r'object/', views.get_objects), #список всех объектов +
-    path(r'object/<int:id>/', views.get_object), # один объект +
     path(r'object/create/', views.create_object), # создание объекта +
-    path(r'object/<int:id>/post/', views.post_object) , #добавление объекта в заявку +
-    path(r'object/<int:id>/put/', views.put_object) , #обновление     объекта +
-    path(r'object/<int:id>/delete/', views.del_object),   #удаление объекта +
+    path(r'object/<int:id>/', views1.object),
+    # path(r'object/<int:id>/', views.get_object), # один объект +
+    # path(r'object/<int:id>/post/', views.post_object) , #добавление объекта в заявку +
+    # path(r'object/<int:id>/put/', views.put_object) , #обновление     объекта +
+    # path(r'object/<int:id>/delete/', views.del_object),   #удаление объекта +
     
     # path(r'object/filter/', views.filter_object), # фильтр +
     # path(r'expedition/filter_exp/', views.filter_exp), # фильтр +
     # API Exp
     path(r'expedition/', views.get_exps), #список всех экспедиций +
-    path(r'expedition/<int:id>/', views.get_exp), # одна экспедиция +       
     path(r'expedition/<int:id>/update_user/', views.put_user), # изменение статуса юзером +
     path(r'expedition/<int:id>/update_mod/', views.put_mod), # изменение статуса модератором +
-    path(r'expedition/<int:id>/put/', views.put_exp) , #обновление  экспедиции +
-    path(r'expedition/<int:id>/delete/', views.del_exp), # удаление экспедиции +
-    
-    #path(r'expedition/<int:id>/delete1/', views.del_exp1), # удаление экспедиции +
+    path(r'expedition/<int:id>/', views1.exp), # одна экспедиция +       
     path(r'expedition/<int:id>/delete_exp/<int:id2>', views.del_object_exp),   #удаление объекта из заявки +
+    # path(r'expedition/<int:id>/', views.get_exp), # одна экспедиция +       
+    # path(r'expedition/<int:id>/put/', views.put_exp) , #обновление  экспедиции +
+    # path(r'expedition/<int:id>/delete/', views.del_exp), # удаление экспедиции +
+    #path(r'expedition/<int:id>/delete1/', views.del_exp1), # удаление экспедиции +
     # API Exp_Obj
-    #path(r'Programm/', views.get_pros) , #получение списка  м-м +
     path(r'Programm/<str:id>/', views.get_pro) , #получение списка  м-м +
     path(r'Programm/<str:id>/put/', views.put_pro), #обновление объекта +
+    #path(r'Programm/', views.get_pros) , #получение списка  м-м +
     #path(r'Programm/<str:id>/delete/', views.del_pro), #удаление объекта +
     
 
-       
 ]   
---
+++ ExploreSib/lab/views1.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render
+from .models import *
+import psycopg2
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from .serializers import *
+from rest_framework.decorators import api_view
+import ast
+from django.http import HttpResponse
+from django.db.models.query import QuerySet
+
+
+@api_view(['Get', 'Post', 'Delete','Put'])
+def object(request,id,format=None):
+    """
+    Возвращает объект
+    """
+    if request.method == 'GET':
+        if not Object.objects.filter(ID_Object=id).exists():
+            return Response(f"Объекта с таким id нет")
+        obj = get_object_or_404(Object, ID_Object=id)
+        #obj = City_Obj.objects.get(ID_Object=id)
+        print(obj)
+        serializer = ObjSerializer(obj)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        """
+        Обновляет объект
+        """
+        obj = get_object_or_404(Object, ID_Object=id)
+        #print('hi',obj)
+        serializer = ObjSerializer(obj,data=request.data)
+        print('se',serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'POST':
+        """
+        Добавляет объект в заявку 
+        """
+
+        if not Object.objects.filter(ID_Object=id).exists():
+            return Response(f"Объекта с таким id нет")
+        obj = Object.objects.get(ID_Object = id)
+        print('ob', obj)
+        exp = Expedition.objects.filter(Status='in').last()
+        print('exp',exp)
+        if exp is None:
+            exp = Expedition.objects.create()
+        print('exp',exp)
+        exp.Objects.add(obj)
+        exp.save()
+
+        serializer = ExpSerializer(exp)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        """
+        Удаляет объект
+        """
+        obj = get_object_or_404(Object, ID_Object=id)
+        obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
+        return HttpResponse('error')    
+    
+    
+@api_view(['Get', 'Post', 'Delete','Put'])
+def exp(request,id,format=None):
+    
+    if request.method == 'GET':
+        """
+        Возвращает экспедицию
+        """
+        if request.method == 'GET':
+            obj = get_object_or_404(Expedition, ID_Expedition=id)
+            #obj = City_Obj.objects.get(ID_Object=id)
+            print(obj)
+            serializer = ExpSerializer(obj)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        """
+        Обновляет экспедицию
+        """
+        obj = get_object_or_404(Expedition, ID_Expedition=id)
+        print('ob',obj)
+        serializer = ExpSerializer(obj,data=request.data)
+        print('se',serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'POST':
+        return HttpResponse('error no post') 
+    elif request.method == 'DELETE':
+        """
+        Удаляет экспедицию
+        """
+        obj = get_object_or_404(Expedition, ID_Expedition=id)
+        print('ob',obj)
+        obj.delete() 
+
+        exp = Expedition.objects.all()
+        serializer = ExpSerializer(exp, many=True)
+        return Response(serializer.data)
+    else:
+        return HttpResponse('error')    
