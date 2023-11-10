647ab59f3b67cc929ea864bbf45f9a4ae0dd50da
daniilsmiirnov <dan_smirnov@mail.ru>
2023-11-10 13:24:34 +0300

    swagger 0.1

---
+++ ExploreSib/lab/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path 
+from . import views 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # API objects
+    path(r'object/', views.get_objects), #список всех объектов +
+    path(r'object/create/', views.create_object), # создание объекта +
+    path(r'object/<int:id>/', views1.object),
+
+    # API Exp
+    path(r'expedition/', views.get_exps), #список всех экспедиций +
+    path(r'expedition/<int:id>/update_user/', views.put_user), # изменение статуса юзером +
+    path(r'expedition/<int:id>/update_mod/', views.put_mod), # изменение статуса модератором +
+    path(r'expedition/<int:id>/', views1.exp), # одна экспедиция +       
+    path(r'expedition/<int:id>/delete_exp/<int:id2>', views.del_object_exp),   #удаление объекта из заявки +
+
+    # API Exp_Obj
+    path(r'Programm/<str:id>/', views.get_pro) , #получение списка  м-м +
+
+] 
